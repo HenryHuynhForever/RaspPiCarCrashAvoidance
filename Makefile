f5ea51c541e5e1ecb79ce68f799d3546116ff1da
HenryHuynhForever <0248997@schoolsnet.act.edu.au>
2023-08-31 10:14:56 +1000

    Added optimisation flags.

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
-main:
-	g++ main.cpp -o MyProgram
+CXX=g++
+CXXFLAGS=-Wall -march=native -O3 -pipe
+
+main: main.cpp
+	$(CXX) main.cpp -o MyProgram $(CXXFLAGS)
+	@echo "Compilation finished"
--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
-#include "./bcm2835/bcm2835.h"
-
 
 int main(){
-	std::cout<<"Hello world!"<<std::endl;
+	std::cout << "Hello world!" << std::endl;
 	return 0;
 }
-
-
-
