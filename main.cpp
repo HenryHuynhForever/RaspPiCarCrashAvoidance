84ef09c41087e7e6a32e3749647db2fe6f84475b
HenryHuynhForever <0248997@schoolsnet.act.edu.au>
2023-09-11 11:23:34 +1000

    Implemented frame resizing and grayscale.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace cv;
 
@@ -25,8 +26,14 @@ int main(){
 		if (frame.empty()){
 			break;
 		}
+		
+		Mat resizePass;
+		Mat colorPass;
 
-		imshow("Live", frame);
+		resize(frame, resizePass, resizePass.size(), 0.5, 0.5, INTER_NEAREST);
+		cvtColor(resizePass, colorPass, COLOR_BGR2GRAY);
+
+		imshow("Live", colorPass);
 
 		if (waitKey(5) >= 0){
 			break;
